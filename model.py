6aa1c3dca282f2774f415d2d86592368995882e2
mmoham12 <mmoham12@uncc.edu>
2017-06-22 15:16:42 -0400

    MAc- New Model.py

--- model.py
+++ model.py
@@ -473,83 +473,112 @@ class DCGAN(object):
                 return tf.nn.sigmoid(h3), h3
 
     def generator(self, z, y=None):
-#         s_h, s_w = self.output_height, self.output_width
-# s_h2, s_w2 = conv_out_size_same(s_h, 2), conv_out_size_same(s_w, 2)
-# s_h4, s_w4 = conv_out_size_same(s_h2, 2), conv_out_size_same(s_w2, 2)
-# s_h8, s_w8 = conv_out_size_same(s_h4, 2), conv_out_size_same(s_w4, 2)
-# s_h16, s_w16 = conv_out_size_same(s_h8, 2), conv_out_size_same(s_w8, 2)
-# s_h16, s_w16 = conv_out_size_same(s_h8, 2), conv_out_size_same(s_w8, 2)
-        with tf.variable_scope("generator") as scope:
-            if not self.y_dim:
-                s_h, s_w = self.output_height, self.output_width
-                s_h2, s_w2 = conv_out_size_same(
-                    s_h, 2), conv_out_size_same(s_w, 2)
-                s_h4, s_w4 = conv_out_size_same(
-                    s_h2, 2), conv_out_size_same(s_w2, 2)
-                s_h8, s_w8 = conv_out_size_same(
-                    s_h4, 2), conv_out_size_same(s_w4, 2)
-                s_h16, s_w16 = conv_out_size_same(
-                    s_h8, 2), conv_out_size_same(s_w8, 2)
+        
+        s_h, s_w = self.output_height, self.output_width
+        s_h2, s_w2 = conv_out_size_same(s_h, 2), conv_out_size_same(s_w, 2)
+        s_h4, s_w4 = conv_out_size_same(s_h2, 2), conv_out_size_same(s_w2, 2)
+        s_h8, s_w8 = conv_out_size_same(s_h4, 2), conv_out_size_same(s_w4, 2)
+        s_h16, s_w16 = conv_out_size_same(s_h8, 2), conv_out_size_same(s_w8, 2)
 
-                # project `z` and reshape
-                self.z_, self.h0_w, self.h0_b = linear(
-                    z, self.gf_dim * 8 * s_h16 * s_w16, 'g_h0_lin', with_w=True)
+        # project `z` and reshape
+        yb = tf.reshape(y, [self.batch_size, 1, 1, self.y_dim])
+        z = concat([z, y], 1)
+        self.z_, self.h0_w, self.h0_b = linear(z, self.gf_dim * 8 * s_h16 * s_w16, 'g_h0_lin', with_w=True)
 
-                self.h0 = tf.reshape(
-                    self.z_, [-1, s_h16, s_w16, self.gf_dim * 8])
-                h0 = tf.nn.relu(self.g_bn0(self.h0))
+        self.h0 = tf.reshape(self.z_, [-1, s_h16, s_w16, self.gf_dim * 8])
+        h0 = tf.nn.relu(self.g_bn0(self.h0))
+        h0 = conv_cond_concat(h0, yb)
 
-                self.h1, self.h1_w, self.h1_b = deconv2d(
-                    h0, [self.batch_size, s_h8, s_w8, self.gf_dim * 4], name='g_h1', with_w=True)
-                h1 = tf.nn.relu(self.g_bn1(self.h1))
+        self.h1, self.h1_w, self.h1_b = deconv2d(h0, [self.batch_size, s_h8, s_w8, self.gf_dim * 4], name='g_h1',
+                                                with_w=True)
+        h1 = tf.nn.relu(self.g_bn1(self.h1))
+        h1 = conv_cond_concat(h1, yb)
 
-                h2, self.h2_w, self.h2_b = deconv2d(
-                    h1, [self.batch_size, s_h4, s_w4, self.gf_dim * 2], name='g_h2', with_w=True)
-                h2 = tf.nn.relu(self.g_bn2(h2))
+        h2, self.h2_w, self.h2_b = deconv2d(
+            h1, [self.batch_size, s_h4, s_w4, self.gf_dim * 2], name='g_h2', with_w=True)
+        h2 = tf.nn.relu(self.g_bn2(h2))
+        h2 = conv_cond_concat(h2, yb)
 
-                h3, self.h3_w, self.h3_b = deconv2d(
-                    h2, [self.batch_size, s_h2, s_w2, self.gf_dim * 1], name='g_h3', with_w=True)
-                h3 = tf.nn.relu(self.g_bn3(h3))
+        h3, self.h3_w, self.h3_b = deconv2d(
+            h2, [self.batch_size, s_h2, s_w2, self.gf_dim * 1], name='g_h3', with_w=True)
+        h3 = tf.nn.relu(self.g_bn3(h3))
+        h3 = conv_cond_concat(h3, yb)
 
-                h4, self.h4_w, self.h4_b = deconv2d(
-                    h3, [self.batch_size, s_h, s_w, self.c_dim], name='g_h4', with_w=True)
+        h4, self.h4_w, self.h4_b = deconv2d(
+            h3, [self.batch_size, s_h, s_w, self.c_dim], name='g_h4', with_w=True)
 
-                return tf.nn.tanh(h4)
-            else:
-                s_h, s_w = self.output_height, self.output_width
-                s_h2, s_h4 = int(s_h / 2), int(s_h / 4)
-                s_w2, s_w4 = int(s_w / 2), int(s_w / 4)
+        return tf.nn.tanh(h4)
+        # with tf.variable_scope("generator") as scope:
+        #     if not self.y_dim:
+        #         s_h, s_w = self.output_height, self.output_width
+        #         s_h2, s_w2 = conv_out_size_same(
+        #             s_h, 2), conv_out_size_same(s_w, 2)
+        #         s_h4, s_w4 = conv_out_size_same(
+        #             s_h2, 2), conv_out_size_same(s_w2, 2)
+        #         s_h8, s_w8 = conv_out_size_same(
+        #             s_h4, 2), conv_out_size_same(s_w4, 2)
+        #         s_h16, s_w16 = conv_out_size_same(
+        #             s_h8, 2), conv_out_size_same(s_w8, 2)
 
-                # yb = tf.expand_dims(tf.expand_dims(y, 1),2)
-                print( "tf reshape 1= " +str(self.y_dim))
+        #         # project `z` and reshape
+        #         self.z_, self.h0_w, self.h0_b = linear(
+        #             z, self.gf_dim * 8 * s_h16 * s_w16, 'g_h0_lin', with_w=True)
 
-                yb = tf.reshape(y, [self.batch_size, 1, 1, self.y_dim])
-                z = concat([z, y], 1) #MM z =[ batch_size , z_dim + y_dim]
+        #         self.h0 = tf.reshape(
+        #             self.z_, [-1, s_h16, s_w16, self.gf_dim * 8])
+        #         h0 = tf.nn.relu(self.g_bn0(self.h0))
+
+        #         self.h1, self.h1_w, self.h1_b = deconv2d(
+        #             h0, [self.batch_size, s_h8, s_w8, self.gf_dim * 4], name='g_h1', with_w=True)
+        #         h1 = tf.nn.relu(self.g_bn1(self.h1))
+
+        #         h2, self.h2_w, self.h2_b = deconv2d(
+        #             h1, [self.batch_size, s_h4, s_w4, self.gf_dim * 2], name='g_h2', with_w=True)
+        #         h2 = tf.nn.relu(self.g_bn2(h2))
+
+        #         h3, self.h3_w, self.h3_b = deconv2d(
+        #             h2, [self.batch_size, s_h2, s_w2, self.gf_dim * 1], name='g_h3', with_w=True)
+        #         h3 = tf.nn.relu(self.g_bn3(h3))
+
+        #         h4, self.h4_w, self.h4_b = deconv2d(
+        #             h3, [self.batch_size, s_h, s_w, self.c_dim], name='g_h4', with_w=True)
+
+        #         return tf.nn.tanh(h4)
+        #     else:
+        #         s_h, s_w = self.output_height, self.output_width
+        #         s_h2, s_h4 = int(s_h / 2), int(s_h / 4)
+        #         s_w2, s_w4 = int(s_w / 2), int(s_w / 4)
+
+        #         # yb = tf.expand_dims(tf.expand_dims(y, 1),2)
+        #         print( "tf reshape 1= " +str(self.y_dim))
+
+        #         yb = tf.reshape(y, [self.batch_size, 1, 1, self.y_dim])
+        #         z = concat([z, y], 1) #MM z =[ batch_size , z_dim + y_dim]
 
                 
-                h0 = tf.nn.relu(
-                    self.g_bn0(linear(z, self.gfc_dim, 'g_h0_lin')))
-                h0 = concat([h0, y], 1) #MM h0 =[ h0.rows , ho_columns + y_dim]
+        #         h0 = tf.nn.relu(
+        #             self.g_bn0(linear(z, self.gfc_dim, 'g_h0_lin')))
+        #         h0 = concat([h0, y], 1) #MM h0 =[ h0.rows , ho_columns + y_dim]
 
                 
-                h1 = tf.nn.relu(self.g_bn1(
-                    linear(h0, self.gf_dim * 2 * s_h4 * s_w4, 'g_h1_lin')))
-                h1 = tf.reshape(
-                    h1, [self.batch_size, s_h4, s_w4, self.gf_dim * 2])
+        #         h1 = tf.nn.relu(self.g_bn1(
+        #             linear(h0, self.gf_dim * 2 * s_h4 * s_w4, 'g_h1_lin')))
+        #         h1 = tf.reshape(
+        #             h1, [self.batch_size, s_h4, s_w4, self.gf_dim * 2])
 
                
-                h1 = conv_cond_concat(h1, yb)
+        #         h1 = conv_cond_concat(h1, yb)
 
                
 
-                h2 = tf.nn.relu(self.g_bn2(deconv2d(h1,
-                                                    [self.batch_size, s_h2, s_w2, self.gf_dim * 2], name='g_h2')))
+        #         h2 = tf.nn.relu(self.g_bn2(deconv2d(h1,
+        #                                             [self.batch_size, s_h2, s_w2, self.gf_dim * 2], name='g_h2')))
                 
-                h2 = conv_cond_concat(h2, yb)
+        #         h2 = conv_cond_concat(h2, yb)
 
                 
-                return tf.nn.sigmoid(
-                    deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3'))
+        #         return tf.nn.sigmoid(
+        #             deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3'))
 
     def sampler(self, z, y=None):
         with tf.variable_scope("generator") as scope:
